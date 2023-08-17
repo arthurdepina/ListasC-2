9009052352cb2d8b10066fba0deb507653bfcd84
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-08-16 23:41:02 -0300

    Exercício 04

--- testes.c
+++ testes.c
@@ -9,22 +9,6 @@ float fatorial(int n){
     }
 }
 
-int produto(int a, int b){
-    if (b == 1){
-        return a;
-    }else{
-        return produto(a, (b - 1)) + a;
-    }
-}
-
-int somaImpares(int n){
-    if(n == 1){
-        return 1;
-    }else{
-        return (2 * n - 1) + somaImpares(n - 1);
-    }
-}
-
 float xToTheNth(int x, int n){
     if (n == 0) {
         return 1;
@@ -36,16 +20,14 @@ float xToTheNth(int x, int n){
 float quartaQuestao(float x, float n) {
     if (n == 0) {
         return 1;
+    } else if (n == 1) {
+        return x + quartaQuestao(x, n - 1);
     } else {
         return xToTheNth(x, n)/fatorial(n) + quartaQuestao(x, n - 1);
     }
 }
 
 int main(void) {
-    printf("%.0f\n", fatorial(5));
-    printf("%d\n", produto(8, 12));
-    printf("%d\n", somaImpares(4));
-    printf("%.0f\n", xToTheNth(5, 4));
-    printf("%.4f\n", quartaQuestao(5, 4));
+    printf("%f\n", quartaQuestao(5, 4));
     return 0;
 }
