121330459b3164e22daf6c7f31b6a4ee91b2bd6b
Arthur <arthurdepinabalduinoleitao@gmail.com>
2023-08-25 11:55:18 -0300

    correcting l02e03

--- l02_NotWorking.c
+++ l02_NotWorking.c
@@ -18,13 +18,16 @@ int mdc_2(int x, int y){
 
 int potencia(int x, int n){
 	if(n == 0) return 1;
-	if(n > 0 && n%2 == 0) return potencia(potencia(x, n/2), 2);
+	if(n > 0 && n%2 == 0){  
+           int r = potencia(x, n/2);
+           return r*r;
+        }
 	if(n > 0 && n%2 != 0) return x*potencia(potencia(x, (n-1)/2), 2);
 	return 0;
 }
 
 int main(){
-	printf("%d\n", mdc(270, 192));
+	/*printf("%d\n", mdc(270, 192));
 	printf("%d\n", mdc(35, 10));
 	printf("%d\n", mdc(10, 15));
 	printf("%d\n", mdc(31, 2));
@@ -33,7 +36,7 @@ int main(){
 	printf("%d\n", mdc_2(35, 10));
 	printf("%d\n", mdc_2(10, 15));
 	printf("%d\n", mdc_2(31, 2));
-	printf("==================\n");
+	printf("==================\n");*/
 	printf("%d\n", potencia(2, 3));
 	printf("%d\n", potencia(3, 4));
 	printf("%d\n", potencia(8, 0));
