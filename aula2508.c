1abadfb5a7fd278f27eabb50cee327d67d852069
Arthur <arthurdepinabalduinoleitao@gmail.com>
2023-08-25 11:54:58 -0300

    Create aula2508.c

---
+++ aula2508.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void ex2(int a, int b){
+	if (b == 1) return a;
+	if (b%2 = 0) return ex2(a*a, b/2);
+	return a * ex2(a - 1, b - 1);
+}
+
+int f(int n){ // Ex3
+	if(n <= 1) return n;
+	else return f(n-1) + f(n-2);
+}
+
+void hanoi(int n, char orig, char dest, char aux){
+	if (n > 0) { 
+		hanoi(n - 1, orig, aux, dest);
+	}
+	printf("mover %c para %c\n", orig, dest);
+	hanoi(n-1, aux, dest, orig);
+}
+
+// Teorema mestre
+
+void g(int n) {
+	if(n > 1) {
+		for (i = 0; i < n; i++){
+			printf("%d", n);
+		}
+		g(n/2); g(n/2); g(n/2); g(n/2);
+	}
+ } // theta(n^2)
+
+void h(int n){
+	if (n > 1) {
+		for (i = 0; i < n; i++){
+			for (j = 0; j < n; j++){
+				printf("%d\n");
+			}
+		}
+		f(n/4); f(n/4); f(n/4);
+	}
+}
+
+
+int main(){
+	
+	return 0;
+}
