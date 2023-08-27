44c662f27097a244cf5eb72810d4b2653747913c
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-08-27 13:53:51 -0300

    useful.c

--- useful.c
+++ useful.c
@@ -7,3 +7,21 @@ void exibir_vetor(int *v, int len){
 		else printf("%d, ", v[i]);
 	}
 }
+
+void exibir_matriz(int **matriz, int n_linhas, int n_colunas){
+    int i, j;
+    for (i = 0; i < n_linhas; i++){
+        for (j = 0; j < n_colunas; j++){
+            printf("[%d][%d] = %d\n", i, j, matriz[i][j]);
+        }
+    }
+}
+
+int **alocar_matriz(int n_linhas, int n_colunas){
+    int **matriz;
+    matriz = (int**) calloc(n_linhas, sizeof(int*));
+    for (int i = 0; i < n_linhas; i++){
+        matriz[i] = (int *) calloc(n_colunas, sizeof(int));
+    }
+    return matriz;
+}
