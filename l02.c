0cd95e3e2df23cdd2fc5c52ad7d7069e7dbc91e9
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-08-31 21:18:16 -0300

    Correções estéticas

--- l02.c
+++ l02.c
@@ -10,22 +10,22 @@ void exibir_vetor(int *v, int len){
 
 int mdc(int x, int y){
 	if (y <= x && x%y == 0) return y;
-	if (x < y) return mdc(y, x);
+	if (x < y)              return mdc(y, x);
 	return mdc(y, x%y);
 }
 
 int mdc_2(int x, int y){
 	if (x == y) return x;
-	if (x%2 == 0 && y%2 == 0) return 2*(mdc_2(x/2, y/2));
-	if (x%2 == 0 && y%2 != 0) return mdc_2(x/2, y);
-	if (x%2 != 0 && y%2 == 0) return mdc_2(x, y/2);
+	if (x%2 == 0 && y%2 == 0)          return 2*(mdc_2(x/2, y/2));
+	if (x%2 == 0 && y%2 != 0)          return mdc_2(x/2, y);
+	if (x%2 != 0 && y%2 == 0)          return mdc_2(x, y/2);
 	if (x%2 != 0 && y%2 != 0 && x > y) return mdc_2((x-y)/2, y);
 	if (x%2 != 0 && y%2 != 0 && x < y) return mdc_2(x, (y-x)/2);
 	return 0;
 }
 
 int potencia(int x, int n){
-	if(n == 0) return 1;
+	if(n == 0)            return 1;
 	if(n > 0 && n%2 == 0) return potencia(x, n/2) * potencia(x, n/2);
 	if(n > 0 && n%2 != 0) return x * potencia(x, n/2) * potencia(x, n/2);
 	return 0;
@@ -47,34 +47,34 @@ int max_bitonic(int *vetor, int menor, int maior){
   
 int main(){
 	printf("==01==\n");
-	printf("%d\n", mdc(270, 192));
-	printf("%d\n", mdc(35, 10));
-	printf("%d\n", mdc(10, 15));
-	printf("%d\n", mdc(31, 2));
+	printf("%d\n",  mdc(270, 192));
+	printf("%d\n",    mdc(35, 10));
+	printf("%d\n",    mdc(10, 15));
+	printf("%d\n",     mdc(31, 2));
 	printf("==02==\n");
 	printf("%d\n", mdc_2(270, 192));
-	printf("%d\n", mdc_2(35, 10));
-	printf("%d\n", mdc_2(10, 15));
-	printf("%d\n", mdc_2(31, 2));
+	printf("%d\n",   mdc_2(35, 10));
+	printf("%d\n",   mdc_2(10, 15));
+	printf("%d\n",    mdc_2(31, 2));
 	printf("==03==\n");
 	printf("%d\n", potencia(2, 3));
 	printf("%d\n", potencia(3, 4));
 	printf("%d\n", potencia(8, 0));
 	printf("%d\n", potencia(9, 3));
 	printf("==04==\n");
-	printf("%d\n", coef_binomial(10, 8));
+	printf("%d\n",  coef_binomial(10, 8));
 	printf("%d\n", coef_binomial(42, 40));
 	printf("%d\n", coef_binomial(24, 20));
-	printf("%d\n", coef_binomial(4, 2));
+	printf("%d\n",   coef_binomial(4, 2));
 	printf("=====================05=====================\n");
 	int vetor_a[] = {1, 3, 5, 13, 12, 11, 9, 8, 4, 2};
 	int vetor_b[] = {1, 5, 13, 17, 19, 20, 21, 22, 16, 14, 4, 2};
 	int vetor_c[] = {2, 4, 5, 8, 11, 30};
 	int vetor_d[] = {10, 9, 8, 7, 6, 4, 2};
-	int len_a = sizeof(vetor_a)/sizeof(vetor_a[0]);
-	int len_b = sizeof(vetor_b)/sizeof(vetor_b[0]);
-	int len_c = sizeof(vetor_c)/sizeof(vetor_c[0]);
-	int len_d = sizeof(vetor_d)/sizeof(vetor_d[0]);
+	int len_a     = sizeof(vetor_a)/sizeof(vetor_a[0]);
+	int len_b     = sizeof(vetor_b)/sizeof(vetor_b[0]);
+	int len_c     = sizeof(vetor_c)/sizeof(vetor_c[0]);
+	int len_d     = sizeof(vetor_d)/sizeof(vetor_d[0]);
 	exibir_vetor(vetor_a, len_a); printf("\n");
 	printf("Máximo elemento: %d\n", max_bitonic(vetor_a, 0, len_a-1));
 	exibir_vetor(vetor_b, len_b); printf("\n");
--- l03.c
+++ l03.c
@@ -2,11 +2,7 @@
 #include <stdbool.h>
 
 bool verifica_se_menor(int *r, int *s, int len_a, int len_b){
-        int size_r = sizeof(r) / sizeof(r[0]);
-        // printf("Size R (A): %d\n", size_r);
-        int size_s = sizeof(s) / sizeof(s[0]);
-        // printf("Size B: %d\n", size_s);
-        for (int i = 0; i < size_r; i++){
+        for (int i = 0; i < len_a; i++){
                 if (r[i] > s[i]) return false;
                 if (r[i] < s[i]) return true;
         if (len_a > len_b) return false;
@@ -14,13 +10,14 @@ bool verifica_se_menor(int *r, int *s, int len_a, int len_b){
 	}
 }
 
+
+
 int main(){
-        int a[] = {4, 1, 2, 5, 6, 9};
+        int a[] = {4, 1, 2, 5, 6, 9, 10, 11};
         int size_a = sizeof(a) /  sizeof(a[0]);
-        // printf("Size A: %d\n", size_a);
-        int b[] = {4, 1, 2, 5, 6, 9, 8};
+        int b[] = {4, 1, 2, 5, 6, 9};
         int size_b = sizeof(b) / sizeof(b[0]);
-        // printf("Size B: %d\n", size_b);
         printf("01: %s\n", verifica_se_menor(a, b, size_a, size_b) ? "V: A < B" : "F: A > B");
+        
         return 0;
 }
