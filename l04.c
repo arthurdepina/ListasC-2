3170abd9beacfc68c74140e74c04e952f5c46ec1
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-02 17:43:18 -0300

    organizacao

---
+++ l04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void seg_consec(int *a, int len_a){
+    int current = a[0];
+    for (int i = 0; i < len_a; i++){
+        
+    }
+}
+
+int main () {
+    int a[] = {7, 9, 5, 4, 5, 4, 8, 6};
+    int size_a = sizeof(a) /  sizeof(a[0]);
+
+    return 0;
+}
