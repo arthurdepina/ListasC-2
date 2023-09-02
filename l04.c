6185e27f894e938ac17e926c93098c7973ff42d5
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-02 19:39:37 -0300

    L04 E01

--- l04.c
+++ l04.c
@@ -2,15 +2,49 @@
 #include <stdbool.h>
 
 void seg_consec(int *a, int len_a){
-    int current = a[0];
-    for (int i = 0; i < len_a; i++){
-        
+    bool iguais = false;
+    int tamanho = 0;
+    int dist = 0;
+    for (int current = 0; current < len_a; current++){
+        for (int i = current + 1; i < len_a; i++){
+            if (a[i] == a[current]) {
+                dist = i - current;
+                for (int j = current; j < i; j++){
+                    if (a[j] != a[dist + j]) { iguais = false; break; }
+                    else iguais = true; tamanho++;
+                }
+                if (!iguais) continue;
+                if (iguais)
+                    printf("i = %d | m = %d\n", current + 1, tamanho); break;
+            }
+            if (iguais) break;
+        }
+        if (iguais) break;
     }
+    if (!iguais) printf("Sem segmentos consecutivos.\n");
 }
 
 int main () {
+    printf("====== 01 ======\n");
+    printf("a) ");
     int a[] = {7, 9, 5, 4, 5, 4, 8, 6};
     int size_a = sizeof(a) /  sizeof(a[0]);
-
+    seg_consec(a, size_a);
+    printf("b) ");
+    int b[] = {7, 9, 5, 4, 8, 5, 4, 8, 6};
+    int size_b = sizeof(b) /  sizeof(b[0]);
+    seg_consec(b, size_b);
+    printf("c) ");
+    int c[] = {0, 1, 2, 0, 1, 2, 6};
+    int size_c = sizeof(c) / sizeof(c[0]);
+    seg_consec(c, size_c);
+    printf("d) ");
+    int d[] = {2, 2, 3, 5, 6};
+    int size_d = sizeof(d) / sizeof(d[0]);
+    seg_consec(d, size_d);
+    printf("e) ");
+    int e[] = {2, 3, 5, 2, 3, 6};
+    int size_e = sizeof(e) / sizeof(e[0]);
+    seg_consec(e, size_e);
     return 0;
 }
--- testes.c
+++ testes.c
@@ -4,16 +4,8 @@
 
 int main(){
 	int vetor_a[] = {1, 3, 5, 13, 12, 11, 9, 8, 4, 2};
-	int vetor_b[] = {1, 5, 13, 17, 19, 20, 21, 22, 16, 14, 4, 2};
-	int vetor_c[] = {2, 4, 5, 8, 11, 30};
-	int vetor_d[] = {10, 9, 8, 7, 6, 4, 2};
-	int len_a = sizeof(vetor_a)/sizeof(vetor_a[0]);
-	int len_b = sizeof(vetor_b)/sizeof(vetor_b[0]);
-	int len_c = sizeof(vetor_c)/sizeof(vetor_c[0]);
-	int len_d = sizeof(vetor_d)/sizeof(vetor_d[0]);
-	exibir_vetor(vetor_a, len_a); printf("\n");
-	exibir_vetor(vetor_b, len_b); printf("\n");
-	exibir_vetor(vetor_c, len_c); printf("\n");
-	exibir_vetor(vetor_d, len_d); printf("\n");
+	int current = 4;
+	int c = vetor_a[current];
+	printf("%d\n", c);
 	return 0;
 }
