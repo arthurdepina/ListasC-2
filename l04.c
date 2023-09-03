5fc4848894fe5041f6af15b1a5d01aee19cabdd1
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-02 22:35:43 -0300

    bug interessante no testes.c

--- l04.c
+++ l04.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+void exibir_vetor(int *v, int len){
+	for (int i = 0; i < len; i++){
+		if (i == 0) printf("[");
+		if (i == len - 1) printf("%d]", v[i]);
+		else printf("%d, ", v[i]);
+	}
+}
 
 void seg_consec(int *a, int len_a){
     /*
@@ -21,8 +30,10 @@ void seg_consec(int *a, int len_a){
                     else iguais = true; tamanho++;
                 }
                 if (!iguais) continue;
-                if (iguais)
-                    printf("i = %d | m = %d\n", current + 1, tamanho); break;
+                if (iguais) {
+                    printf("i = %d | m = %d\n", current + 1, tamanho);
+                    break;
+                }
             }
             if (iguais) break;
         }
@@ -43,6 +54,20 @@ int max_soma_seg(int *s, int len_s){
         }
     }
     return max;
+}
+
+int * junta_ordenadas(int *a, int *b, int len_a, int len_b){
+    int contador_a = 0, contador_b = 0;
+    int len = len_a + len_b;
+    int *seq = (int *)malloc(len * sizeof(int));
+    for (int i = 0; i < len; i++){
+        if (a[contador_a] < b[contador_b]) {
+            seq[i] = a[contador_a]; contador_a++;
+        } else {
+            seq[i] = b[contador_b]; contador_b++;
+        }
+    }
+    return seq;
 }
 
 int main () {
@@ -80,7 +105,8 @@ int main () {
     int h[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
     int m = sizeof(g) / sizeof(g[0]);
     int n = sizeof(h) / sizeof(h[0]);
-
-
+    int *array; int len_array = m + n;
+    array = junta_ordenadas(g, h, m, n);
+    exibir_vetor(array, len_array);
     return 0;
 }
--- testes.c
+++ testes.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include "useful.c"
 
+int * junta_ordenadas(int *a, int *b, int len_a, int len_b){
+    int contador_a = 0, contador_b = 0;
+    int len = len_a + len_b;
+    int *seq = (int *)malloc(len * sizeof(int));
+    for (int i = 0; i < len; i++){
+        if (a[contador_a] < b[contador_b]) {
+            seq[i] = a[contador_a]; contador_a++;
+        } else {
+            seq[i] = b[contador_b]; contador_b++;
+        }
+    }
+    return seq;
+}
+
 int main () {
-    int f[] = {5, 2, -2, -7, 3, 14, 10, -3, 9, -6, 4, 1};
-    int size_f = sizeof(f) / sizeof(f[0]);
-	int *p;
-	p = teste_retorna_array(f, size_f);
-	exibir_vetor(p, size_f);
+    int g[] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int h[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
+    int m = sizeof(g) / sizeof(g[0]);
+    int n = sizeof(h) / sizeof(h[0]);
+    int *array; int len_array = m + n;
+    array = junta_ordenadas(g, h, m, n);
+    exibir_vetor(array, len_array);
     return 0;
 }
