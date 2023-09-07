3a6470d97b8f195fe192020bf21c09fedae735e5
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-07 19:18:26 -0300

    L05 E01

--- l05.c
+++ l05.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
-#include <math.h>
 
-int converte_p_binario(int n){
-    int bin = 0;
-    int i = 1;
-    int resto;
-    while (n != 0){
-        resto = n % 2;
-        printf("%d\n", resto);
-        n /= 2;
-
+void n_binario(char *bin, int n, int atual){
+    if (atual == n) {
+        bin[atual] = '\0';
+        printf("%s\n", bin);
+        return;
     }
+    
+    bin[atual] = '0';
+    n_binario(bin, n, atual+1);
+
+    bin[atual] = '1';
+    n_binario(bin, n, atual+1);
 }
 
-void n_binario(int n, int atual){
-    int a = pow(n, 2);
-    if (a == atual) { printf("0\n"); return; }
-    if (atual != 1) printf("%d\n", a - atual);
-    n_binario(n, atual+1);
-}
-    
 int main() {
-    n_binario(3, 1);
-    printf("\n");
-    // n_binario(4, 0);
+    printf("====== 01 ======\n");
+    printf("Exemplo, n = 3:\n");
+    int tam_str = 4;
+    char str[tam_str + 1];
+    n_binario(str, tam_str, 0);
+
+    printf("====== 02 ======\n");
+    
+
     return 0;
 }
