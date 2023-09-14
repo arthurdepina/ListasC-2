8f6da9710ae04f36032974d0262c3f66b6495b85
Arthur <arthurdepinabalduinoleitao@gmail.com>
2023-09-13 23:18:56 -0300

    L 05 E 02

--- l05.c
+++ l05.c
@@ -14,9 +14,22 @@ void n_binario(char *bin, int n, int atual){
     n_binario(bin, n, atual+1);
 }
 
-void abba(char *resultado, int m, int n){
-    resultado++;
+void abba(char *resultado, int m, int n, int count_m, int count_n ){
+    if (count_m == m && count_n == n) {
+        resultado[count_m + count_n] = '\0';
+        printf("%s\n", resultado);
+        return;
+    }
     
+    if (count_m < m) {
+        resultado[count_m + count_n] = 'A';
+        abba(resultado, m, n, count_m + 1, count_n);
+    }
+
+    if (count_n <  n) {
+        resultado[count_m + count_n] = 'B';
+        abba(resultado, m, n, count_m, count_n + 1);
+    }
 }
 
 int main() {
@@ -27,7 +40,22 @@ int main() {
     n_binario(str, tam_str, 0);
 
     printf("====== 02 ======\n");
-    
+    int m = 3;                  // Gols de A
+    int n = 3;                  // Gols de B
+    int tam_placar = m + n;
+    char placar[tam_placar + 1];
+    abba(placar, m, n,  0, 0);
+
+    printf("====== 03 ======\n");
+
 
     return 0;
 }
+
+
+
+
+
+
+
+
