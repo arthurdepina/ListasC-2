1d663b735185f830780df9da3cb3f0610850d081
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-09-25 22:34:34 -0300

    Update ListaRevisao01.c
    ex 10

--- Listas/ListaRevisao01.c
+++ Listas/ListaRevisao01.c
@@ -132,6 +132,25 @@ void n_binario(char *bin, int n, int atual){
     n_binario(bin, n, atual+1);
 }
 
+// ex 10
+void abba(char *resultado, int m, int n, int count_m, int count_n ){
+    if (count_m == m && count_n == n) {
+        resultado[count_m + count_n] = '\0';
+        printf("%s\n", resultado);
+        return;
+    }
+    
+    if (count_m < m) {
+        resultado[count_m + count_n] = 'A';
+        abba(resultado, m, n, count_m + 1, count_n);
+    }
+
+    if (count_n <  n) {
+        resultado[count_m + count_n] = 'B';
+        abba(resultado, m, n, count_m, count_n + 1);
+    }
+}
+
 int main()
 {
 	printf("=====================01=====================\n");
@@ -217,4 +236,10 @@ int main()
     printf("Exemplo, n = %d:\n", tam_str - 1);
     char str[tam_str + 1];
     n_binario(str, tam_str, 0);
+	printf("=====================10=====================\n");
+    int gols_a = 3;
+    int gols_b = 3;
+    int tam_placar = gols_a + gols_b;
+    char placar[tam_placar + 1];
+    abba(placar, gols_a, gols_b,  0, 0);
 }
