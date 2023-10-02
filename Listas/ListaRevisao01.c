0a56c7f75b8dc7551a8edb7279a4863a4053d7db
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2023-10-01 22:29:07 -0300

    Update ListaRevisao01.c
    arrumando identação

--- Listas/ListaRevisao01.c
+++ Listas/ListaRevisao01.c
@@ -325,151 +325,151 @@ bool existe_subconjunto(int *conjunto, int len, int soma, int c, int count)
 
 int main()
 {
-	printf("=====================01=====================\n");
+    printf("=====================01=====================\n");
 
-	printf("%d\n", mdc_2(270, 192));
-	printf("%d\n",   mdc_2(35, 10));
-	printf("%d\n",   mdc_2(10, 15));
-	printf("%d\n",    mdc_2(31, 2));
+    printf("%d\n", mdc_2(270, 192));
+    printf("%d\n",   mdc_2(35, 10));
+    printf("%d\n",   mdc_2(10, 15));
+    printf("%d\n",    mdc_2(31, 2));
 
-	printf("=====================02=====================\n");
+    printf("=====================02=====================\n");
 
-	int vetor_a[] = {1, 3, 5, 13, 12, 11, 9, 8, 4, 2};
-	int vetor_b[] = {1, 5, 13, 17, 19, 20, 21, 22, 16, 14, 4, 2};
-	int vetor_c[] = {2, 4, 5, 8, 11, 30};
-	int vetor_d[] = {10, 9, 8, 7, 6, 4, 2};
-	int len_a     = sizeof(vetor_a)/sizeof(vetor_a[0]);
-	int len_b     = sizeof(vetor_b)/sizeof(vetor_b[0]);
-	int len_c     = sizeof(vetor_c)/sizeof(vetor_c[0]);
-	int len_d     = sizeof(vetor_d)/sizeof(vetor_d[0]);
-	exibir_vetor(vetor_a, len_a); printf("\n");
-	printf("Máximo elemento: %d\n", max_bitonic(vetor_a, 0, len_a-1));
-	exibir_vetor(vetor_b, len_b); printf("\n");
-	printf("Máximo elemento: %d\n", max_bitonic(vetor_b, 0, len_b-1));
-	exibir_vetor(vetor_c, len_c); printf("\n");
-	printf("Máximo elemento: %d\n", max_bitonic(vetor_c, 0, len_c-1));
-	exibir_vetor(vetor_d, len_d); printf("\n");
-	printf("Máximo elemento: %d\n", max_bitonic(vetor_d, 0, len_d-1));
+    int vetor_a[] = {1, 3, 5, 13, 12, 11, 9, 8, 4, 2};
+    int vetor_b[] = {1, 5, 13, 17, 19, 20, 21, 22, 16, 14, 4, 2};
+    int vetor_c[] = {2, 4, 5, 8, 11, 30};
+    int vetor_d[] = {10, 9, 8, 7, 6, 4, 2};
+    int len_a     = sizeof(vetor_a)/sizeof(vetor_a[0]);
+    int len_b     = sizeof(vetor_b)/sizeof(vetor_b[0]);
+    int len_c     = sizeof(vetor_c)/sizeof(vetor_c[0]);
+    int len_d     = sizeof(vetor_d)/sizeof(vetor_d[0]);
+    exibir_vetor(vetor_a, len_a); printf("\n");
+    printf("Máximo elemento: %d\n", max_bitonic(vetor_a, 0, len_a-1));
+    exibir_vetor(vetor_b, len_b); printf("\n");
+    printf("Máximo elemento: %d\n", max_bitonic(vetor_b, 0, len_b-1));
+    exibir_vetor(vetor_c, len_c); printf("\n");
+    printf("Máximo elemento: %d\n", max_bitonic(vetor_c, 0, len_c-1));
+    exibir_vetor(vetor_d, len_d); printf("\n");
+    printf("Máximo elemento: %d\n", max_bitonic(vetor_d, 0, len_d-1));
 
-	printf("=====================03=====================\n");
+    printf("=====================03=====================\n");
 
-	float quadrada = recursive_sqrt(13, 3.2, 0.001);
-	printf("a) %f\n", quadrada);
-	float quadrada2 = recursive_sqrt(100, 90, 0.0001);
-	printf("b)%f\n", quadrada2);
+    float quadrada = recursive_sqrt(13, 3.2, 0.001);
+    printf("a) %f\n", quadrada);
+    float quadrada2 = recursive_sqrt(100, 90, 0.0001);
+    printf("b)%f\n", quadrada2);
 
-	printf("=====================04=====================\n");
+    printf("=====================04=====================\n");
 
-	int m[] = {5, 2, 2, 3, 4, 4, 4, 4, 4, 1, 1};
-	int size_m = sizeof(m) / sizeof(m[0]);
-	printf("04: %d\n", conta_sub_seq(m, size_m));
-	int n[] = {3, 3, -1, -1, -1, 12, 12, 12, 3, 3};
-	int size_n = sizeof(n) / sizeof(n[0]);
-	printf("04: %d\n", conta_sub_seq(n, size_n));
+    int m[] = {5, 2, 2, 3, 4, 4, 4, 4, 4, 1, 1};
+    int size_m = sizeof(m) / sizeof(m[0]);
+    printf("04: %d\n", conta_sub_seq(m, size_m));
+    int n[] = {3, 3, -1, -1, -1, 12, 12, 12, 3, 3};
+    int size_n = sizeof(n) / sizeof(n[0]);
+    printf("04: %d\n", conta_sub_seq(n, size_n));
 
-	printf("=====================05=====================\n");
+    printf("=====================05=====================\n");
 
-	int p[] = {5, 10, 3, 2, 4, 7, 9, 8, 5};
-	int size_p = sizeof(p) / sizeof(p[0]);
-	printf("05: %d\n", max_seq_cres(p, size_p));
-	int q[] = {10, 8, 7, 5, 2};
-	int size_q = sizeof(q) / sizeof(q[0]);
-	printf("05: %d\n", max_seq_cres(q, size_q));
+    int p[] = {5, 10, 3, 2, 4, 7, 9, 8, 5};
+    int size_p = sizeof(p) / sizeof(p[0]);
+    printf("05: %d\n", max_seq_cres(p, size_p));
+    int q[] = {10, 8, 7, 5, 2};
+    int size_q = sizeof(q) / sizeof(q[0]);
+    printf("05: %d\n", max_seq_cres(q, size_q));
 
-	printf("=====================06=====================\n");
+    printf("=====================06=====================\n");
 
-	printf("a) ");
-	int a[] = {7, 9, 5, 4, 5, 4, 8, 6};
-	int size_a = sizeof(a) /  sizeof(a[0]);
-	seg_consec(a, size_a);
-	printf("b) ");
-	int b[] = {7, 9, 5, 4, 8, 5, 4, 8, 6};
-	int size_b = sizeof(b) /  sizeof(b[0]);
-	seg_consec(b, size_b);
-	printf("c) ");
-	int c[] = {0, 1, 2, 0, 1, 2, 6};
-	int size_c = sizeof(c) / sizeof(c[0]);
-	seg_consec(c, size_c);
-	printf("d) ");
-	int d[] = {2, 2, 3, 5, 6};
-	int size_d = sizeof(d) / sizeof(d[0]);
-	seg_consec(d, size_d);
-	printf("e) ");
-	int e[] = {2, 3, 5, 2, 3, 6};
-	int size_e = sizeof(e) / sizeof(e[0]);
-	seg_consec(e, size_e);
+    printf("a) ");
+    int a[] = {7, 9, 5, 4, 5, 4, 8, 6};
+    int size_a = sizeof(a) /  sizeof(a[0]);
+    seg_consec(a, size_a);
+    printf("b) ");
+    int b[] = {7, 9, 5, 4, 8, 5, 4, 8, 6};
+    int size_b = sizeof(b) /  sizeof(b[0]);
+    seg_consec(b, size_b);
+    printf("c) ");
+    int c[] = {0, 1, 2, 0, 1, 2, 6};
+    int size_c = sizeof(c) / sizeof(c[0]);
+    seg_consec(c, size_c);
+    printf("d) ");
+    int d[] = {2, 2, 3, 5, 6};
+    int size_d = sizeof(d) / sizeof(d[0]);
+    seg_consec(d, size_d);
+    printf("e) ");
+    int e[] = {2, 3, 5, 2, 3, 6};
+    int size_e = sizeof(e) / sizeof(e[0]);
+    seg_consec(e, size_e);
 
-	printf("=====================07=====================\n");
+    printf("=====================07=====================\n");
 
-	int f[] = {5, 2, -2, -7, 3, 14, 10, -3, 9, -6, 4, 1};
-	int size_f = sizeof(f) / sizeof(f[0]);
-	printf("%d\n", max_soma_seg(f, size_f));
+    int f[] = {5, 2, -2, -7, 3, 14, 10, -3, 9, -6, 4, 1};
+    int size_f = sizeof(f) / sizeof(f[0]);
+    printf("%d\n", max_soma_seg(f, size_f));
 
-	printf("=====================08=====================\n");
+    printf("=====================08=====================\n");
 
-	int g[] = {2, 4, 6, 8, 10, 12, 14, 16};
-	int h[] = {1, 3, 5, 7, 9, 11, 13, 15};
-	int size_g = sizeof(g) / sizeof(g[0]);
-	int size_h = sizeof(h) / sizeof(h[0]);
-	int *array; int len_array = size_g + size_h;
-	array = junta_ordenadas(g, h, size_g, size_h);
-	exibir_vetor(array, len_array); printf("\n");
-	free(array);
-	int p_8[] = {18, 19};
-	int q_8[] = {3, 6, 7, 9, 11, 14, 15, 16};
-	int len_p_8 = sizeof(p_8) / sizeof(p_8[0]);
-	int len_q_8 = sizeof(q_8) / sizeof(q_8[0]);
-	int *array2; int len_array2 = len_p_8 + len_q_8;
-	array2 = junta_ordenadas(p_8, q_8, len_p_8, len_q_8);
-	exibir_vetor(array2, len_array2); printf("\n");
-	free(array2);
+    int g[] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int h[] = {1, 3, 5, 7, 9, 11, 13, 15};
+    int size_g = sizeof(g) / sizeof(g[0]);
+    int size_h = sizeof(h) / sizeof(h[0]);
+    int *array; int len_array = size_g + size_h;
+    array = junta_ordenadas(g, h, size_g, size_h);
+    exibir_vetor(array, len_array); printf("\n");
+    free(array);
+    int p_8[] = {18, 19};
+    int q_8[] = {3, 6, 7, 9, 11, 14, 15, 16};
+    int len_p_8 = sizeof(p_8) / sizeof(p_8[0]);
+    int len_q_8 = sizeof(q_8) / sizeof(q_8[0]);
+    int *array2; int len_array2 = len_p_8 + len_q_8;
+    array2 = junta_ordenadas(p_8, q_8, len_p_8, len_q_8);
+    exibir_vetor(array2, len_array2); printf("\n");
+    free(array2);
 
-	printf("=====================09=====================\n");
+    printf("=====================09=====================\n");
 
-	int tam_str = 3;
-	printf("Exemplo, n = %d:\n", tam_str - 1);
-	char str[tam_str + 1];
-	n_binario(str, tam_str, 0);
-	
-	printf("=====================10=====================\n");
+    int tam_str = 3;
+    printf("Exemplo, n = %d:\n", tam_str - 1);
+    char str[tam_str + 1];
+    n_binario(str, tam_str, 0);
 
-	int gols_a = 3;
-	int gols_b = 3;
-	int tam_placar = gols_a + gols_b;
-	char placar[tam_placar + 1];
-	abba(placar, gols_a, gols_b,  0, 0);
+    printf("=====================10=====================\n");
 
-	printf("=====================11=====================\n");
+    int gols_a = 3;
+    int gols_b = 3;
+    int tam_placar = gols_a + gols_b;
+    char placar[tam_placar + 1];
+    abba(placar, gols_a, gols_b,  0, 0);
 
-	int array_sort[] = {3, 1, 4, 1, 5, 9, 2};
-	int len_array_sort = sizeof(array_sort) / sizeof(array_sort[0]);
-	printf("Vetor original: ");
-	exibir_vetor(array_sort, len_array_sort); printf("\n");
-	ordena_rec(array_sort, 0, len_array_sort);
-	printf("Vetor ordenado: ");
-	exibir_vetor(array_sort, len_array_sort); printf("\n");
+    printf("=====================11=====================\n");
 
-	printf("=====================12=====================\n");
+    int array_sort[] = {3, 1, 4, 1, 5, 9, 2};
+    int len_array_sort = sizeof(array_sort) / sizeof(array_sort[0]);
+    printf("Vetor original: ");
+    exibir_vetor(array_sort, len_array_sort); printf("\n");
+    ordena_rec(array_sort, 0, len_array_sort);
+    printf("Vetor ordenado: ");
+    exibir_vetor(array_sort, len_array_sort); printf("\n");
+
+    printf("=====================12=====================\n");
 
     int palitos[] = {9, 8, 7, 6, 4, 3, 2, 1};
     int len_palitos = sizeof(palitos) / sizeof(palitos[0]);
     if (quadrado_palito(palitos, len_palitos)) printf("Possível montar quadrado\n");
     else printf("Impossível montar quadrado\n");
 
-	printf("=====================13=====================\n");
+    printf("=====================13=====================\n");
 
     int inicio_x = 0;
     int inicio_y = 0;
     percurso_cavalo(inicio_x, inicio_y);
 
-	printf("=====================14=====================\n");
-	
+    printf("=====================14=====================\n");
+
     int array_permut[] = {4, 2, 1, 5};
     int len_permut = sizeof(array_permut) / sizeof(array_permut[0]);
     int permut_maxima = max_elegante(array_permut, len_permut, 0);
     printf("Soma permutada elegante = %d\n", permut_maxima);
 
-	printf("=====================15=====================\n");
+    printf("=====================15=====================\n");
 
     int cheques[] = {30, 40, 10, 15, 11, 60, 54};
     int len_cheques = sizeof(cheques) / sizeof(cheques[0]);
@@ -478,6 +478,6 @@ int main()
     if (existe) printf("Há cheques correspondentes.\n");
     else printf("Não há cheques correspondentes.\n");
 
-	printf("============================================\n");
-	return 0;
+    printf("============================================\n");
+    return 0;
 }
